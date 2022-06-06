71ab84f1e9cf1b0397e36a2a1a532e7a6ba13bad
yared2112 <teklewold.yared21@gmail.com>
2022-06-07 02:18:20 +0300

    completed task

---
+++ task-tracker/src/App.js
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello</h1>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ task-tracker/src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <Header>
+        <h1>Hello</h1>
+    </Header>
+  )
+}
+
+export default Header
