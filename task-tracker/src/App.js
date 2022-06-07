e47f3a35fd9ec2be7eff067cec4c2bf6c7664ebc
yared2112 <teklewold.yared21@gmail.com>
2022-06-07 03:03:46 +0300

    completed task

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 function App() {
   return (
     <div className="container">
       <h1>Hello</h1>
       <Header />
       <Tasks />
+      <AddTask />
     </div>
   );
 }
